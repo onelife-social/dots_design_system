6b2691aa98d627488a5469b4f38fb9d03079902c
juanan-onelife <juanan.garcia@onelife.social>
2025-05-27 18:27:40 +0200

    fix merge

--- lib/src/components/components_lib.dart
+++ lib/src/components/components_lib.dart
@@ -1,1 +1,3 @@
 export 'buttons/buttons_lib.dart';
+export 'progress_bar/progress_bar.dart';
+export 'action_sheet/action_sheet.dart';
--- lib/src/components/progress_bar/dots_progress_bar.dart
+++ lib/src/components/progress_bar/dots_progress_bar.dart
@@ -1,4 +1,4 @@
-import 'package:dots_design_system/theme/dots_theme.dart';
+import 'package:dots_design_system/dots_design_system.dart';
 import 'package:flutter/material.dart';
 
 /// Conventional progress but divided in parts
