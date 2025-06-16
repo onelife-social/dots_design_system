b49eb079811844cbce7a364a9b23ad1d71bdf70f
vsanblasgomez <vsanblasgomez@gmail.com>
2025-06-16 14:24:58 +0200

    feat(topbar): allow children widgets in title variant of DotsHomeTopBar

--- lib/src/components/navigation/home_top_bar.dart
+++ lib/src/components/navigation/home_top_bar.dart
@@ -32,41 +32,53 @@ class DotsHomeTopBar extends StatelessWidget implements PreferredSizeWidget {
   /// Whether to hide the background of the top bar.
   final bool showGradient;
 
+  /// Whether to apply a background blur effect.
+  final bool backgroundBlur;
+
+  /// additional widgets to display in the top bar.
+  final List<Widget>? children;
+
   final bool _bigStatusBar;
 
   /// Only status bar, no title or segmented control.
   const DotsHomeTopBar.onlyStatusBar({
     super.key,
     this.showGradient = false,
+    this.backgroundBlur = false,
   })  : title = null,
         child = null,
+        children = null,
         imgProfile = null,
         rightIcon = null,
         secondRightIcon = null,
         _bigStatusBar = false;
 
-  /// Bar with title,left image profile and right icons.
+  /// Bar with title,left image profile and right icons (and additional widgets).
   const DotsHomeTopBar.title({
     super.key,
-    this.showGradient = false,
+    this.backgroundBlur = false,
     required String this.title,
     this.imgProfile,
     this.rightIcon,
     this.secondRightIcon,
+    this.children,
   })  : 
         child = null,
+        showGradient = false,
         _bigStatusBar = true;
 
-  /// Bar with widget, left image profile and right icons.
+  /// Bar with widget, only central child
   const DotsHomeTopBar.widget({
     super.key,
     this.showGradient = false,
+    this.backgroundBlur = false,
     this.child,
-    this.imgProfile,
-    this.rightIcon,
-    this.secondRightIcon,
   })  : 
+        children = null,
         title = null,
+        imgProfile = null,
+        rightIcon = null,
+        secondRightIcon = null,
         _bigStatusBar = true;
 
   @override
@@ -83,14 +95,14 @@ class DotsHomeTopBar extends StatelessWidget implements PreferredSizeWidget {
     final theme = context.dotsTheme;
     final aspectRatio = MediaQuery.of(context).aspectRatio;
 
-    final content = _TopBarContent(context, theme, aspectRatio);
+    final content = _topBarContent(context, theme, aspectRatio);
 
     return ClipRRect(
       borderRadius: BorderRadius.zero,
       child: ConstrainedBox(
-        constraints: BoxConstraints.tightFor(height: preferredSize.height),
+        constraints: BoxConstraints(minHeight: preferredSize.height),
         child: Stack(
-          fit: StackFit.expand,
+          fit: StackFit.loose,
           children: [
             showGradient
                 ? ShaderMask(
@@ -112,20 +124,31 @@ class DotsHomeTopBar extends StatelessWidget implements PreferredSizeWidget {
                       ),
                     ),
                   )
-                : BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 50, sigmaY: 50),
-                    child: Container(
-                      color: theme.colors.bgContainerPrimary,
-                    ),
+                : Positioned.fill(
+                      child: backgroundBlur
+                        ? BackdropFilter(
+                            filter: ImageFilter.blur(sigmaX: 50, sigmaY: 50),
+                          child: Container(
+                            color: theme.colors.bgContainerPrimary,
+                          ),
+                        )
+                      : Container(
+                          color: Colors.transparent,
+                        ),
                   ),
-            content,
+            Column(
+              children: [
+                  content,
+                if (children != null) ...children!,
+              ],
+            ),
           ],
         ),
       ),
     );
   }
 
-  Widget _TopBarContent(BuildContext context, DotsTheme theme, double aspectRatio) {
+  Widget _topBarContent(BuildContext context, DotsTheme theme, double aspectRatio) {
     return ConstrainedBox(
       constraints: BoxConstraints.tightFor(height: preferredSize.height),
       child: Column(
