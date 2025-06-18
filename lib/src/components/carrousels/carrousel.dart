d914bff63ac5cff3868d3825454f222a3e0bdb7c
Alexei <alexei.jilinskiy@onelife.social>
2025-06-18 12:19:09 +0200

    Fixed issue with MemoryCards not displaying items properly

--- lib/src/components/carrousels/carrousel.dart
+++ lib/src/components/carrousels/carrousel.dart
@@ -1,10 +1,9 @@
 import 'dart:math';
 
-import 'package:dots_design_system/dots_design_system.dart';
 import 'package:flutter/material.dart';
 
 class Carrousel extends StatelessWidget {
-  final List<MemoryCard> memories;
+  final List<Widget> memories;
   const Carrousel({super.key, required this.memories});
 
   static const List<double> rotationPattern = [
