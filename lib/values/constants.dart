0182ee6dc80185435e3d685436527d40fec3336d
andres-sen <andres.sen@onelife.social>
2025-06-26 13:50:06 +0200

    PR fixes

--- lib/values/constants.dart
+++ lib/values/constants.dart
@@ -1,7 +1,7 @@
 import 'package:dots_design_system/dots_design_system.dart' as ds;
 
-@Deprecated('Use kNormalHeight on dots_design_system')
+@Deprecated('Use kTopBarNormalHeight on dots_design_system')
 const kNormalHeight = ds.kTopBarNormalHeight;
 
-@Deprecated('Use kBigHeight on dots_design_system')
+@Deprecated('Use kTopBarBigHeight on dots_design_system')
 const kBigHeight = ds.kTopBarBigHeight;
