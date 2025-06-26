d427f067384a896673adbab3451b72d30423c668
rubengmlp <175928757+rubengmlpz@users.noreply.github.com>
2025-06-26 11:49:30 +0200

    Create featureItem

---
+++ lib/src/components/feature_item/feature_item.dart
@@ -0,0 +1,42 @@
+import 'package:dots_design_system/dots_design_system.dart';
+import 'package:flutter/material.dart';
+
+class FeatureItem extends StatelessWidget {
+  final String feature;
+  final String value;
+  final DotsIconData icon;
+
+  const FeatureItem({super.key, required this.feature, required this.value, required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = context.dotsTheme;
+
+    return Row(
+      spacing: 8,
+      children: [
+        DotsIcon(iconData: icon, size: 16, color: theme.colors.textQuarternary),
+        Expanded(
+          flex: 7,
+          child: Text(
+            feature,
+            style: theme.typo.main.bodyDefaultRegular.copyWith(
+              color: theme.colors.textTertiary,
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ),
+        Expanded(
+          flex: 3,
+          child: Text(
+            value,
+            style: theme.typo.main.bodyDefaultMedium.copyWith(
+              color: theme.colors.textSecondary,
+            ),
+            textAlign: TextAlign.right,
+          ),
+        ),
+      ],
+    );
+  }
+}
