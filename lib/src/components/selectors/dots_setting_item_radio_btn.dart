311d2c0ca9f542436193f5bb30324e34d68e40b6
Alex Luque <alex.luque@onelife.social>
2025-06-30 08:12:00 +0200

    deleted dividers

--- lib/src/components/selectors/dots_setting_item_radio_btn.dart
+++ lib/src/components/selectors/dots_setting_item_radio_btn.dart
@@ -24,60 +24,36 @@ class DotsSettingItemRadioBtn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        CustomDivider(context: context),
-        GestureDetector(
-          onTap: onTap,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                DotsRadioButton(
-                  isSelected: isSelected,
-                  size: 22,
-                ),
-                const SizedBox(width: 12),
-                Text(
-                  leadingLabel,
-                  style: context.dotsTheme.typo.main.bodyDefaultBold,
-                ),
-                const Spacer(),
-                const SizedBox(width: 12),
-                if (trailingLabel != null)
-                  Text(
-                    trailingLabel!,
-                    style: context.dotsTheme.typo.main.bodyDefaultMedium.copyWith(
-                      color: context.dotsTheme.colors.textTertiary,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-              ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            DotsRadioButton(
+              isSelected: isSelected,
+              size: 22,
             ),
-          ),
+            const SizedBox(width: 12),
+            Text(
+              leadingLabel,
+              style: context.dotsTheme.typo.main.bodyDefaultBold,
+            ),
+            const Spacer(),
+            const SizedBox(width: 12),
+            if (trailingLabel != null)
+              Text(
+                trailingLabel!,
+                style: context.dotsTheme.typo.main.bodyDefaultMedium.copyWith(
+                  color: context.dotsTheme.colors.textTertiary,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+          ],
         ),
-        CustomDivider(context: context),
-      ],
-    );
-  }
-}
-
-class CustomDivider extends StatelessWidget {
-  final BuildContext context;
-
-  const CustomDivider({super.key, required this.context});
-
-  @override
-  Widget build(BuildContext context) {
-    return Divider(
-      height: 0,
-      thickness: 0.5,
-      color: context.dotsTheme.colors.labelSecondary.dotsWithOpacity(0.3),
-      indent: 44.3,
-      endIndent: 16.0,
+      ),
     );
   }
 }
