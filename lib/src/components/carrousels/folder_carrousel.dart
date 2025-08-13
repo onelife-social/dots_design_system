64c9e2a6c8b869fb5a6acc8ac61b5b9f68b8c282
Alexei <alexei.jilinskiy@onelife.social>
2025-08-13 16:47:10 +0200

    Added custom widgets to folder carrousel

--- lib/src/components/carrousels/folder_carrousel.dart
+++ lib/src/components/carrousels/folder_carrousel.dart
@@ -2,16 +2,17 @@ import 'package:dots_design_system/dots_design_system.dart';
 import 'package:flutter/material.dart';
 
 class FolderCarrousel extends StatelessWidget {
+  /// Array of button data to build buttons automatically with a [BtnFolder] widget
   final List<BtnFolderData> buttonsData;
+
+  /// List of widgets to render directly inside the folder carrousel which override ButtonsData
+  final List<Widget>? customWidgets;
 
   // Variable to determine if the edit icon should be shown.
   final bool showEditIcon;
 
-  const FolderCarrousel({
-    super.key,
-    required this.buttonsData,
-    required this.showEditIcon,
-  });
+  const FolderCarrousel(
+      {super.key, required this.buttonsData, required this.showEditIcon, this.customWidgets});
 
   @override
   Widget build(BuildContext context) {
@@ -20,20 +21,21 @@ class FolderCarrousel extends StatelessWidget {
       child: SingleChildScrollView(
         scrollDirection: Axis.horizontal,
         child: Row(
-          children: [
-            for (int i = 0; i < buttonsData.length; i++) ...[
-              BtnFolder(
-                icon: buttonsData[i].icon,
-                text: buttonsData[i].text,
-                onPressed: buttonsData[i].onPressed,
-                isSelected: buttonsData[i].isSelected,
-                iconSelectedColor: buttonsData[i].iconSelectedColor,
-                isEditable: buttonsData[i].isEditable,
-                showEditIcon: showEditIcon,
-              ),
-              if (i != buttonsData.length - 1) const SizedBox(width: 6),
-            ],
-          ],
+          children: customWidgets ??
+              [
+                for (int i = 0; i < buttonsData.length; i++) ...[
+                  BtnFolder(
+                    icon: buttonsData[i].icon,
+                    text: buttonsData[i].text,
+                    onPressed: buttonsData[i].onPressed,
+                    isSelected: buttonsData[i].isSelected,
+                    iconSelectedColor: buttonsData[i].iconSelectedColor,
+                    isEditable: buttonsData[i].isEditable,
+                    showEditIcon: showEditIcon,
+                  ),
+                  if (i != buttonsData.length - 1) const SizedBox(width: 6),
+                ],
+              ],
         ),
       ),
     );
