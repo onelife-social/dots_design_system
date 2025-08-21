33b6f3210d1e0088cba38a5ef21c01d7d99f52b2
vsanblasgomez <vsanblasgomez@gmail.com>
2025-08-21 11:39:03 +0200

    Added on tap to element

--- lib/src/components/notifications/activity_preview.dart
+++ lib/src/components/notifications/activity_preview.dart
@@ -35,9 +35,12 @@ class ActivityPreview extends StatelessWidget {
   /// for example: '123' or '1.2K' and the 'K' depends on the idiom.
   final String? number;
 
+  /// Callback when the card is tapped.
+  final Function()? onCardTap;
 
   const ActivityPreview({
     this.variant = ActivityPreviewVariant.reactions,
+    this.onCardTap,
     super.key,
     this.image,
     this.onError,
@@ -49,85 +52,88 @@ class ActivityPreview extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = context.dotsTheme;
 
-    return AspectRatio(
-      aspectRatio: 3 / 4,
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 10),
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: theme.colors.bgContainerSecondary,
-            width: 1,
-          ),
-          borderRadius: DotsBorderRadius.r24,
-          image: (image != null)
-            ? DecorationImage(
-                image: image!,
-                fit: BoxFit.cover,
-                onError: (exception, stackTrace) {
-                  onError?.call(exception, stackTrace);
-                },
-              )
-            : null,
-          boxShadow: [
-            BoxShadow(
-              color: const Color(0x26000000),
-              blurRadius: 10,
-              offset: const Offset(0, 4),
-              spreadRadius: 0,
+    return GestureDetector(
+      onTap: onCardTap,
+      child: AspectRatio(
+        aspectRatio: 3 / 4,
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 10),
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: theme.colors.bgContainerSecondary,
+              width: 1,
             ),
-          ],
-        ),
-        child: Stack(
-          children: [
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: ClipRRect(
-                borderRadius: DotsBorderRadius.r100,
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10), 
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 10),
-                    decoration: BoxDecoration(
-                      color: theme.colors.bgSecondaryBtn,
-                      borderRadius: DotsBorderRadius.r100,
-                    ),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        if (variant.isReactions && reactions?.isNotEmpty == true)
-                          ...reactions!.map((reaction) => Padding(
-                                padding: const EdgeInsets.only(right: 5),
-                                child: Text(
-                                  reaction,
-                                  style: context.getByRatio(
-                                    theme.typo.number.numLabelDefault, 
-                                    theme.typo.number.numLabelSmall,
+            borderRadius: DotsBorderRadius.r24,
+            image: (image != null)
+              ? DecorationImage(
+                  image: image!,
+                  fit: BoxFit.cover,
+                  onError: (exception, stackTrace) {
+                    onError?.call(exception, stackTrace);
+                  },
+                )
+              : null,
+            boxShadow: [
+              BoxShadow(
+                color: const Color(0x26000000),
+                blurRadius: 10,
+                offset: const Offset(0, 4),
+                spreadRadius: 0,
+              ),
+            ],
+          ),
+          child: Stack(
+            children: [
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: ClipRRect(
+                  borderRadius: DotsBorderRadius.r100,
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10), 
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 10),
+                      decoration: BoxDecoration(
+                        color: theme.colors.bgSecondaryBtn,
+                        borderRadius: DotsBorderRadius.r100,
+                      ),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          if (variant.isReactions && reactions?.isNotEmpty == true)
+                            ...reactions!.map((reaction) => Padding(
+                                  padding: const EdgeInsets.only(right: 5),
+                                  child: Text(
+                                    reaction,
+                                    style: context.getByRatio(
+                                      theme.typo.number.numLabelDefault, 
+                                      theme.typo.number.numLabelSmall,
+                                    ),
                                   ),
-                                ),
-                              )),
-                        if (!variant.isReactions)
-                          DotsIcon(
-                            iconData: variant.isViews
-                                ? DotsIconData.eye
-                                : DotsIconData.heart,
-                            size: 14,
-                            color: theme.colors.labelAlwaysWhite,
+                                )),
+                          if (!variant.isReactions)
+                            DotsIcon(
+                              iconData: variant.isViews
+                                  ? DotsIconData.eye
+                                  : DotsIconData.heart,
+                              size: 14,
+                              color: theme.colors.labelAlwaysWhite,
+                            ),
+                          SizedBox(width: variant.isReactions ? 0 : 4),
+                          Text(
+                            number ?? '',
+                            style: theme.typo.number.numBodyDefault.copyWith(
+                              color: theme.colors.labelAlwaysWhite,
+                            ),
                           ),
-                        SizedBox(width: variant.isReactions ? 0 : 4),
-                        Text(
-                          number ?? '',
-                          style: theme.typo.number.numBodyDefault.copyWith(
-                            color: theme.colors.labelAlwaysWhite,
-                          ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
