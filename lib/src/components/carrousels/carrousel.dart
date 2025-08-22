810a710a0927189099b277264798d7b3ea445cb4
lquiles96 <lydia.quiles@onelife.social>
2025-08-22 12:27:34 +0200

    Delete rotation

--- lib/src/components/carrousels/carrousel.dart
+++ lib/src/components/carrousels/carrousel.dart
@@ -1,33 +1,8 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 class Carrousel extends StatelessWidget {
   final List<Widget> memories;
   const Carrousel({super.key, required this.memories});
-
-  static const List<double> rotationPattern = [
-    2.1,
-    -2.1,
-    0,
-    -1.5,
-    1.3,
-    -0.8,
-    0,
-    2.1,
-    -1.8,
-    0,
-    -2.1,
-    1.6,
-    -0.4,
-    1.9,
-    -1.1,
-    0,
-    2.1,
-    -2.0,
-    0.7,
-    0
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -41,14 +16,9 @@ class Carrousel extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: List.generate(memories.length, (index) {
-              final double rotationAngle = rotationPattern[index % rotationPattern.length];
-
               return Padding(
                 padding: EdgeInsets.only(right: index < memories.length - 1 ? 24 : 0),
-                child: Transform.rotate(
-                  angle: -rotationAngle * (pi / 180),
-                  child: memories[index],
-                ),
+                child: memories[index],
               );
             }),
           ),
