329a5b831fbc5d265f31bb9c3990d962e6c36d4e
Alex Luque <alex.luque@onelife.social>
2025-09-10 16:10:34 +0200

    added radio card icons component

--- example/lib/stories/stories_by_folder/miscellaneous_stories.dart
+++ example/lib/stories/stories_by_folder/miscellaneous_stories.dart
@@ -1,3 +1,4 @@
+import 'package:example/components/radio_card_icons_demo_page.dart';
 import 'package:example/components/radio_card_label_demo_page.dart';
 import 'package:storybook_flutter/storybook_flutter.dart';
 
@@ -6,5 +7,10 @@ List<Story> get miscellaneousStories => [
         name: 'Miscellaneous/Radio Card Label',
         description: 'Demo page for Radio Card Label',
         builder: (context) => RadioCardLabelDemoScreen(),
+      ),
+      Story(
+        name: 'Miscellaneous/Radio Card Icons',
+        description: 'Demo page for Radio Card Icons',
+        builder: (context) => RadioCardIconsDemoScreen(),
       ),
     ];
