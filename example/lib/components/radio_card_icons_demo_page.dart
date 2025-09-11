40173ba6d77630d3a75fbd998d68aea868d56610
Alex Luque <alex.luque@onelife.social>
2025-09-11 16:20:18 +0200

    simplified example

--- example/lib/components/radio_card_icons_demo_page.dart
+++ example/lib/components/radio_card_icons_demo_page.dart
@@ -22,47 +22,41 @@ class _RadioCardIconsDemoScreenState extends State<RadioCardIconsDemoScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            GestureDetector(
-              onTap: () => setState(() => isSelected1 = !isSelected1),
-              child: RadioCardIcons(
-                titleIcon: DotsIconData.gallery,
-                title: 'Título',
-                detailsIcon1: DotsIconData.star,
-                details1:
-                    'Detalles 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-                detailsIcon2: DotsIconData.settings,
-                details2: 'Detalles 2',
-                variant: RadioCardIconsVariant.toggle,
-                isSelected: isSelected1,
-                onTap: () {
-                  setState(() => isSelected1 = !isSelected1);
+            RadioCardIcons(
+              titleIcon: DotsIconData.gallery,
+              title: 'Título',
+              detailsIcon1: DotsIconData.star,
+              details1:
+                  'Detalles 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+              detailsIcon2: DotsIconData.settings,
+              details2: 'Detalles 2',
+              variant: RadioCardIconsVariant.toggle,
+              isSelected: isSelected1,
+              onTap: () {
+                setState(() => isSelected1 = !isSelected1);
 
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Toggle value changed to: $isSelected1')),
-                  );
-                },
-              ),
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Toggle value changed to: $isSelected1')),
+                );
+              },
             ),
             const SizedBox(height: 50),
-            GestureDetector(
-              onTap: () => setState(() => isSelected2 = !isSelected2),
-              child: RadioCardIcons(
-                titleIcon: null,
-                title: 'Título',
-                detailsIcon1: DotsIconData.trash,
-                details1: 'Detalles 1 ',
-                detailsIcon2: null,
-                details2: null,
-                variant: RadioCardIconsVariant.selector,
-                isSelected: isSelected2,
-                onTap: () {
-                  setState(() => isSelected2 = !isSelected2);
+            RadioCardIcons(
+              titleIcon: null,
+              title: 'Título',
+              detailsIcon1: DotsIconData.trash,
+              details1: 'Detalles 1 ',
+              detailsIcon2: null,
+              details2: null,
+              variant: RadioCardIconsVariant.selector,
+              isSelected: isSelected2,
+              onTap: () {
+                setState(() => isSelected2 = !isSelected2);
 
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Selector value changed to: $isSelected2')),
-                  );
-                },
-              ),
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Selector value changed to: $isSelected2')),
+                );
+              },
             ),
           ],
         ),
