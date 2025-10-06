f57f2b004ccaa453c61d30e55019c8d57462b1ba
rubengmlp <175928757+rubengmlpz@users.noreply.github.com>
2025-10-06 17:51:33 +0200

    refactor

--- lib/src/components/buttons/dots_icon_button/dots_icon_button.dart
+++ lib/src/components/buttons/dots_icon_button/dots_icon_button.dart
@@ -192,22 +192,20 @@ class _IconButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final iconWidget = DotsIcon(
+      iconData: icon,
+      size: iconSize ?? size.iconSize,
+      color: color ?? buttonTheme.foregroundColor,
+    );
+
     if (noButtonSize) {
       return Center(
         child: tag != null
             ? BadgeTag(
                 tag: tag!,
-                child: DotsIcon(
-                  iconData: icon,
-                  size: iconSize ?? size.iconSize,
-                  color: color ?? buttonTheme.foregroundColor,
-                ),
+                child: iconWidget,
               )
-            : DotsIcon(
-                iconData: icon,
-                size: iconSize ?? size.iconSize,
-                color: color ?? buttonTheme.foregroundColor,
-              ),
+            : iconWidget,
       );
     }
 
@@ -230,17 +228,9 @@ class _IconButton extends StatelessWidget {
               child: tag != null
                   ? BadgeTag(
                       tag: tag!,
-                      child: DotsIcon(
-                        iconData: icon,
-                        size: iconSize ?? size.iconSize,
-                        color: color ?? buttonTheme.foregroundColor,
-                      ),
+                      child: iconWidget,
                     )
-                  : DotsIcon(
-                      iconData: icon,
-                      size: iconSize ?? size.iconSize,
-                      color: color ?? buttonTheme.foregroundColor,
-                    ),
+                  : iconWidget,
             ),
           ),
         ),
