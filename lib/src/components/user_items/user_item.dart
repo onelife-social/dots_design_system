e63ba4a4d1ee213bce68de893adcd619f5058697
rubengmlp <175928757+rubengmlpz@users.noreply.github.com>
2025-10-14 17:00:42 +0200

    add shadow

--- lib/src/components/user_items/user_item.dart
+++ lib/src/components/user_items/user_item.dart
@@ -22,69 +22,72 @@ class UserItem extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: DotsDecoratedBox(
-        styleType: context.dotsTheme.styles.squircle24,
-        decoration: BoxDecoration(
-          color: context.dotsTheme.colors.bgStrong,
-        ),
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6.5),
-          width: 170,
-          height: 45,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            spacing: 8,
-            children: [
-              Container(
-                width: 26,
-                height: 26,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: context.dotsTheme.colors.bgStrong,
-                ),
-                child: ClipOval(
-                  child: Image(
-                    image: imageProvider,
-                    width: 26,
-                    height: 26,
-                    fit: BoxFit.cover,
-                    errorBuilder: (context, error, stackTrace) {
-                      return DotsIcon(
-                        iconData: DotsIconData.user,
-                        size: 16,
-                        color: context.dotsTheme.colors.textPrimary,
-                      );
-                    },
+        styleType: context.dotsTheme.styles.floatingBtnShadow,
+        child: DotsDecoratedBox(
+          styleType: context.dotsTheme.styles.squircle24,
+          decoration: BoxDecoration(
+            color: context.dotsTheme.colors.bgStrong,
+          ),
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6.5),
+            width: 170,
+            height: 45,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              spacing: 8,
+              children: [
+                Container(
+                  width: 26,
+                  height: 26,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: context.dotsTheme.colors.bgStrong,
+                  ),
+                  child: ClipOval(
+                    child: Image(
+                      image: imageProvider,
+                      width: 26,
+                      height: 26,
+                      fit: BoxFit.cover,
+                      errorBuilder: (context, error, stackTrace) {
+                        return DotsIcon(
+                          iconData: DotsIconData.user,
+                          size: 16,
+                          color: context.dotsTheme.colors.textPrimary,
+                        );
+                      },
+                    ),
                   ),
                 ),
-              ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      name,
-                      style: context.dotsTheme.typo.main.bodyDefaultMedium.copyWith(
-                        color: context.dotsTheme.colors.textPrimary,
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        name,
+                        style: context.dotsTheme.typo.main.bodyDefaultMedium.copyWith(
+                          color: context.dotsTheme.colors.textPrimary,
+                        ),
+                        overflow: TextOverflow.ellipsis,
                       ),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    Text(
-                      details,
-                      style: context.dotsTheme.typo.main.labelSmallRegular.copyWith(
-                        color: context.dotsTheme.colors.textQuarternary,
+                      Text(
+                        details,
+                        style: context.dotsTheme.typo.main.labelSmallRegular.copyWith(
+                          color: context.dotsTheme.colors.textQuarternary,
+                        ),
+                        overflow: TextOverflow.ellipsis,
                       ),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              DotsIcon(
-                iconData: DotsIconData.addCircle,
-                size: 20,
-                color: context.dotsTheme.colors.labelHighlight,
-              ),
-            ],
+                DotsIcon(
+                  iconData: DotsIconData.addCircle,
+                  size: 20,
+                  color: context.dotsTheme.colors.labelHighlight,
+                ),
+              ],
+            ),
           ),
         ),
       ),
