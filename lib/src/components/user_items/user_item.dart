cdc9995c4af8f03c4e08227a31083ab476b5d1a6
rubengmlp <175928757+rubengmlpz@users.noreply.github.com>
2025-10-15 13:27:33 +0200

    Add default image

--- lib/src/components/user_items/user_item.dart
+++ lib/src/components/user_items/user_item.dart
@@ -1,5 +1,7 @@
-import 'package:dots_design_system/dots_design_system.dart';
 import 'package:flutter/material.dart';
+
+import '../../../dots_design_system.dart';
+import '../../core/values/paths/images_paths.dart';
 
 class UserItem extends StatelessWidget {
   const UserItem({
@@ -26,72 +28,84 @@ class UserItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
-      child: DotsDecoratedBox(
-        styleType: context.dotsTheme.styles.floatingBtnShadow,
+      child: SizedBox(
+        height: 45,
+        width: 170,
         child: DotsDecoratedBox(
-          styleType: context.dotsTheme.styles.squircle24,
-          decoration: BoxDecoration(
-            color: context.dotsTheme.colors.bgStrong,
-          ),
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6.5),
-            width: 170,
-            height: 45,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              spacing: 8,
-              children: [
-                Container(
-                  width: 26,
-                  height: 26,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: context.dotsTheme.colors.bgStrong,
-                  ),
-                  child: ClipOval(
-                    child: Image(
-                      image: imageProvider,
-                      width: 26,
-                      height: 26,
-                      fit: BoxFit.cover,
-                      errorBuilder: (context, error, stackTrace) {
-                        return DotsIcon(
-                          iconData: DotsIconData.user,
-                          size: 16,
-                          color: context.dotsTheme.colors.textPrimary,
-                        );
-                      },
+          styleType: context.dotsTheme.styles.floatingBtnShadow,
+          child: DotsDecoratedBox(
+            styleType: context.dotsTheme.styles.squircle24,
+            decoration: BoxDecoration(
+              color: context.dotsTheme.colors.bgStrong,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6.5),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                spacing: 8,
+                children: [
+                  Container(
+                    width: 26,
+                    height: 26,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: context.dotsTheme.colors.bgStrong,
+                    ),
+                    child: ClipOval(
+                      child: Image(
+                        image: imageProvider,
+                        width: 26,
+                        height: 26,
+                        fit: BoxFit.cover,
+                        loadingBuilder: (context, child, loadingProgress) {
+                          if (loadingProgress == null) return child;
+                          return Image.asset(
+                            ImagesPaths.defaultUserItem,
+                            width: 26,
+                            height: 26,
+                            fit: BoxFit.cover,
+                          );
+                        },
+                        errorBuilder: (context, error, stackTrace) {
+                          return Image.asset(
+                            ImagesPaths.defaultUserItem,
+                            width: 26,
+                            height: 26,
+                            fit: BoxFit.cover,
+                          );
+                        },
+                      ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        name,
-                        style: context.dotsTheme.typo.main.bodyDefaultMedium.copyWith(
-                          color: context.dotsTheme.colors.textPrimary,
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          name,
+                          style: context.dotsTheme.typo.main.bodyDefaultMedium.copyWith(
+                            color: context.dotsTheme.colors.textPrimary,
+                          ),
+                          overflow: TextOverflow.ellipsis,
                         ),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      Text(
-                        details,
-                        style: context.dotsTheme.typo.main.labelSmallRegular.copyWith(
-                          color: context.dotsTheme.colors.textQuarternary,
+                        Text(
+                          details,
+                          style: context.dotsTheme.typo.main.labelSmallRegular.copyWith(
+                            color: context.dotsTheme.colors.textQuarternary,
+                          ),
+                          overflow: TextOverflow.ellipsis,
                         ),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                DotsIcon(
-                  iconData: DotsIconData.addCircle,
-                  size: 20,
-                  color: context.dotsTheme.colors.labelHighlight,
-                ),
-              ],
+                  DotsIcon(
+                    iconData: DotsIconData.addCircle,
+                    size: 20,
+                    color: context.dotsTheme.colors.labelHighlight,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
