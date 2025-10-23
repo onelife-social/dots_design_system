fafacaac3f2941c70c2f315b392181856678480c
rubengmlp <175928757+rubengmlpz@users.noreply.github.com>
2025-10-23 16:49:56 +0200

    revert

--- lib/src/components/action_sheet/dots_action_sheet_base.dart
+++ lib/src/components/action_sheet/dots_action_sheet_base.dart
@@ -13,9 +13,7 @@ class DotsActionSheetBase extends StatelessWidget {
   final double? maxHeight;
   final bool showBlurBackground;
   final VoidCallback? onClose;
-  final VoidCallback? onTapCloseButton;
-  final bool addTitlePadding;
-  final TextStyle? titleStyle;
+
   const DotsActionSheetBase({
     super.key,
     required this.title,
@@ -27,9 +25,6 @@ class DotsActionSheetBase extends StatelessWidget {
     this.maxHeight,
     this.showBlurBackground = true,
     this.onClose,
-    this.onTapCloseButton,
-    this.addTitlePadding = false,
-    this.titleStyle,
   });
 
   @override
@@ -61,50 +56,30 @@ class DotsActionSheetBase extends StatelessWidget {
               constraints: BoxConstraints(
                 maxHeight: maxHeight ?? context.screenHeight * 0.8,
               ),
-              child: Stack(
-                children: [
-                  Container(
-                    width: double.infinity,
-                    padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
-                    clipBehavior: Clip.antiAlias,
-                    decoration: ShapeDecoration(
-                      color: theme.colors.bgBaseContrast,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(32),
-                      ),
-                    ),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        _Header(
-                            title: title,
-                            subtitle: subtitle,
-                            onBackButtonTap: onBackButtonTap,
-                            titleStyle: titleStyle,
-                            addTitlePadding: addTitlePadding),
-                        SizedBox(
-                          height: 16,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                          child: child,
-                        ),
-                      ],
-                    ),
+              child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+                clipBehavior: Clip.antiAlias,
+                decoration: ShapeDecoration(
+                  color: theme.colors.bgBaseContrast,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(32),
                   ),
-                  if (onTapCloseButton != null)
-                    Positioned(
-                      top: 16,
-                      right: 16,
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 6),
-                        child: DotsCloseButton(
-                          size: DotsCloseButtonSize.medium,
-                          onTap: onTapCloseButton,
-                        ),
-                      ),
-                    )
-                ],
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    _Header(title: title, subtitle: subtitle, onBackButtonTap: onBackButtonTap),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Expanded(
+                        child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: child,
+                    )),
+                  ],
+                ),
               ),
             ),
           ),
@@ -118,21 +93,17 @@ class _Header extends StatelessWidget {
   final String title;
   final String? subtitle;
   final Function()? onBackButtonTap;
-  final bool addTitlePadding;
-  final TextStyle? titleStyle;
   const _Header({
     required this.title,
     required this.subtitle,
     required this.onBackButtonTap,
-    required this.addTitlePadding,
-    this.titleStyle,
   });
 
   @override
   Widget build(BuildContext context) {
     final theme = context.dotsTheme;
     return SizedBox(
-      height: subtitle != null ? 60 : 50,
+      height: subtitle != null ? 60 : 40,
       child: Column(
         children: [
           Expanded(
@@ -155,17 +126,9 @@ class _Header extends StatelessWidget {
                   left: 0,
                   right: 0,
                   child: Center(
-                    child: Padding(
-                      padding: addTitlePadding
-                          ? const EdgeInsets.symmetric(horizontal: 36)
-                          : EdgeInsets.zero,
-                      child: Text(
-                        title,
-                        style: titleStyle ??
-                            theme.typo.secondary.title02H6
-                                .copyWith(color: theme.colors.textPrimary),
-                        textAlign: TextAlign.center,
-                      ),
+                    child: Text(
+                      title,
+                      style: theme.typo.secondary.title02H6,
                     ),
                   ),
                 ),
@@ -175,9 +138,7 @@ class _Header extends StatelessWidget {
           if (subtitle != null)
             Text(
               subtitle ?? '',
-              style: theme.typo.main.labelDefaultRegular.copyWith(
-                color: theme.colors.textSecondary,
-              ),
+              style: theme.typo.main.labelDefaultRegular,
             ),
         ],
       ),
