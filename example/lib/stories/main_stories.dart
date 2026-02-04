e0d16a51958ad44f61e150ad377ae74a3d3f1524
Alex Luque <alex.luque@onelife.social>
2026-02-04 10:38:45 +0100

    added new milestones submenu

--- example/lib/stories/main_stories.dart
+++ example/lib/stories/main_stories.dart
@@ -22,6 +22,7 @@ import 'package:example/stories/stories_by_folder/countdown_stories.dart';
 import 'package:example/stories/stories_by_folder/dropdown_stories.dart';
 import 'package:example/stories/stories_by_folder/group_cards_stories.dart';
 import 'package:example/stories/stories_by_folder/memory_details.dart';
+import 'package:example/stories/stories_by_folder/milestones_stories.dart';
 import 'package:example/stories/stories_by_folder/miscellaneous_stories.dart';
 import 'package:example/stories/stories_by_folder/notifications_stories.dart';
 import 'package:example/stories/stories_by_folder/photos_stories.dart';
@@ -65,6 +66,7 @@ List<Story> get allStories => [
       ...topBarStories,
       ...uploadItemsStories,
       ...ratingStories,
+      ...milestonesStories,
       Story(
         name: 'Container',
         description: 'Demo page for container',
---
+++ example/lib/stories/stories_by_folder/milestones_stories.dart
@@ -0,0 +1,22 @@
+import 'package:dots_design_system/dots_design_system.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:storybook_flutter/storybook_flutter.dart';
+
+List<Story> get milestonesStories => [
+      Story(
+        name: 'Milestones/Suggested Milestone',
+        description: 'Demo page for Suggested Milestone',
+        builder: (context) => SizedBox(),
+      ),
+      Story(
+        name: 'Milestones/Milestone Card',
+        description: 'Demo page for Milestone Card',
+        builder: (context) => SizedBox(),
+      ),
+      Story(
+        name: 'Milestones/Milestones List',
+        description: 'Demo page for Milestones List',
+        builder: (context) => SizedBox(),
+      ),
+    ];
