c341acfcbe385b912cac932ea44dd8749378feb6
Alex Luque <alex.luque@onelife.social>
2026-02-06 13:45:16 +0100

    deleted suggested milestone from story

--- example/lib/stories/stories_by_folder/milestones_stories.dart
+++ example/lib/stories/stories_by_folder/milestones_stories.dart
@@ -1,15 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:dots_design_system/dots_design_system.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:storybook_flutter/storybook_flutter.dart';
 
 List<Story> get milestonesStories => [
-      Story(
-        name: 'Milestones/Suggested Milestone',
-        description: 'Demo page for Suggested Milestone',
-        builder: (context) => SizedBox(),
-      ),
       Story(
         name: 'Milestones/Milestone Card',
         description: 'Demo page for Milestone Card',
