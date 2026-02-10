1dc23a2f7a1efcca36e680e55290f7d96d7b1bdb
Victor San Blas Gomez <vsanblasgomez@gmail.com>
2026-02-10 14:01:44 +0100

    Fixed coments

--- lib/src/components/cards/dot_book_cards/testimonial_card.dart
+++ lib/src/components/cards/dot_book_cards/testimonial_card.dart
@@ -20,8 +20,8 @@ class DotBookTestimonialCard extends StatelessWidget {
   /// Username for the testimonial.
   final String? username;
 
-	/// Optional tap callback.
-	final VoidCallback? onTap;
+  /// Optional tap callback.
+  final Function()? onTap;
 
 	/// Optional width for the card.
 	final double width;
@@ -41,7 +41,7 @@ class DotBookTestimonialCard extends StatelessWidget {
 		this.title,
 		this.description,
     this.username,
-		this.onTap,
+    this.onTap,
 		this.width = 300,
 		this.onErrorImageCard,
     this.onErrorProfileImage,
@@ -52,94 +52,97 @@ class DotBookTestimonialCard extends StatelessWidget {
 	Widget build(BuildContext context) {
 		final theme = context.dotsTheme;
 
-		return SizedBox(
-			width: width,
-			child: Container(
-				padding: EdgeInsets.only(left: 8, right: 8, top: 8, bottom: 20),
-				decoration: BoxDecoration(
-					color: theme.colors.bgStrong,
-					borderRadius: DotsBorderRadius.r32,
-					boxShadow: [
-						BoxShadow(
-							color: theme.colors.shadowPrimary,
-							blurRadius: 10,
-							offset: const Offset(0, 5),
-							spreadRadius: 0,
-						),
-					],
-				),
-				child: Column(
-					spacing: 18,
-					mainAxisSize: MainAxisSize.min,
-					crossAxisAlignment: CrossAxisAlignment.start,
-					children: [
-            imageCard != null
-            ? ClipRRect(
-                borderRadius: DotsBorderRadius.r24,
-                child: Image(
-                  image: imageCard!,
-                  height: imageHeight,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                  errorBuilder: (context, error, stackTrace) {
-                    if (onErrorImageCard != null) onErrorImageCard!(error, stackTrace);
-                    return SizedBox();
-                  },
-                ),
-              )
-            : SizedBox(),
-						Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  if (title != null && title!.isNotEmpty)...[
-                    Text(
-                      title!,
-                      style: theme.typo.main.titleH5.copyWith(
-                        color: theme.colors.textPrimary,
-                      ),
-                    ),
-                    const SizedBox(height: 8),
-                  ],
-                  if (description != null && description!.isNotEmpty)
-                    Text(
-                      description!,
-                      style: theme.typo.main.bodyLargeRegular.copyWith(
-                        color: theme.colors.textPrimary,
-                      ),
-                    ),
-                ],
+		return GestureDetector(
+      onTap: onTap,
+      child: SizedBox(
+        width: width,
+        child: Container(
+          padding: const EdgeInsets.only(left: 8, right: 8, top: 8, bottom: 20),
+          decoration: BoxDecoration(
+            color: theme.colors.bgStrong,
+            borderRadius: DotsBorderRadius.r32,
+            boxShadow: [
+              BoxShadow(
+                color: theme.colors.shadowPrimary,
+                blurRadius: 10,
+                offset: const Offset(0, 5),
+                spreadRadius: 0,
               ),
-            ),
-						Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12.0),
-              child: Row(
-                children: [
-                  if (imageProfile != null)...[
-                    DotsProfilePhoto(
-                      imageProvider: imageProfile!,
-                      width: 40,
-                      height: 40,
-                      onError: (error, stackTrace) {
-                        if (onErrorProfileImage != null) onErrorProfileImage!(error, stackTrace);
+            ],
+          ),
+          child: Column(
+            spacing: 18,
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              imageCard != null
+                ? ClipRRect(
+                    borderRadius: DotsBorderRadius.r24,
+                    child: Image(
+                      image: imageCard!,
+                      height: imageHeight,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                      errorBuilder: (context, error, stackTrace) {
+                        if (onErrorImageCard != null) onErrorImageCard!(error, stackTrace);
+                        return const SizedBox();
                       },
                     ),
-                    const SizedBox(width: 8),
-                  ],
-                  if (username != null && username!.isNotEmpty)
-                    Text(
-                      username!,
-                      style: theme.typo.main.bodyLargeMedium.copyWith(
-                        color: theme.colors.textTertiary,
+                  )
+                : const SizedBox(),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    if (title != null && title!.isNotEmpty)...[
+                      Text(
+                        title!,
+                        style: theme.typo.main.titleH5.copyWith(
+                          color: theme.colors.textPrimary,
+                        ),
                       ),
-                    ),
-                ],
+                      const SizedBox(height: 8),
+                    ],
+                    if (description != null && description!.isNotEmpty)
+                      Text(
+                        description!,
+                        style: theme.typo.main.bodyLargeRegular.copyWith(
+                          color: theme.colors.textPrimary,
+                        ),
+                      ),
+                  ],
+                ),
               ),
-            ),
-					],
-				),
-			),
-		);
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                child: Row(
+                  children: [
+                    if (imageProfile != null)...[
+                      DotsProfilePhoto(
+                        imageProvider: imageProfile!,
+                        width: 40,
+                        height: 40,
+                        onError: (error, stackTrace) {
+                          if (onErrorProfileImage != null) onErrorProfileImage!(error, stackTrace);
+                        },
+                      ),
+                      const SizedBox(width: 8),
+                    ],
+                    if (username != null && username!.isNotEmpty)
+                      Text(
+                        username!,
+                        style: theme.typo.main.bodyLargeMedium.copyWith(
+                          color: theme.colors.textTertiary,
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
 	}
 }
