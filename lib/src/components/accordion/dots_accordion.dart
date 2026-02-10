14dbf612324348e574727bdfdc404703ed136f88
juanan-onelife <juanan.garcia@onelife.social>
2026-02-10 16:10:52 +0100

    fix color

--- lib/src/components/accordion/dots_accordion.dart
+++ lib/src/components/accordion/dots_accordion.dart
@@ -83,8 +83,10 @@ class _DotsAccordionState extends State<DotsAccordion> with TickerProviderStateM
         for (int i = 0; i < widget.sections.length; i++) ...[
           Divider(
             height: 3,
-            color: theme.colors.labelSecondary,
+            color: theme.colors.labelSecondary.dotsWithOpacity(0.5),
+            thickness: 0.2,
           ),
+
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16),
             child: _SectionHeader(
