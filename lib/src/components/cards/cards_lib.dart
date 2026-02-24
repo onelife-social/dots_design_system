2c895658f07cdbd832d6d462784e06f1ece79cc3
Victor San Blas Gomez <vsanblasgomez@gmail.com>
2026-02-24 15:46:21 +0100

    Delivery Card component

--- lib/src/components/cards/cards_lib.dart
+++ lib/src/components/cards/cards_lib.dart
@@ -4,3 +4,4 @@ export 'recap/recap_lib.dart';
 export 'create_card/create_card_lib.dart';
 export 'dot_book_cards/feature_card.dart';
 export 'dot_book_cards/testimonial_card.dart';
+export 'dot_book_cards/delivery_card.dart';
---
+++ lib/src/components/cards/dot_book_cards/delivery_card.dart
@@ -0,0 +1,119 @@
+import 'package:dots_design_system/dots_design_system.dart';
+import 'package:flutter/material.dart';
+
+class DotBookDeliveryCard extends StatelessWidget {
+
+  /// The image to be displayed on the card.
+  final ImageProvider? image;
+
+  /// The width of the image.
+  final double imageWidth;
+
+  /// The title of the delivery card.
+  final String? title;
+
+  /// The standard delivery text.
+  final String? standarText;
+
+  /// The standard delivery date.
+  final String? standarDate;
+
+  /// The express delivery text.
+  final String? expressText;
+
+  /// The express delivery date.
+  final String? expressDate;
+
+  const DotBookDeliveryCard({
+    super.key,
+    this.image,
+    this.imageWidth = 53,
+    this.title,
+    this.standarText,
+    this.standarDate,
+    this.expressText,
+    this.expressDate,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = context.dotsTheme;
+    
+    return Container(
+      decoration: BoxDecoration(
+        color: theme.colors.bgContainerSecondaryOnBackground,
+        borderRadius: DotsBorderRadius.r32,
+      ),
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          spacing: 24,
+          children: [
+            if (image != null)
+              Image(
+                image: image!,
+                width: imageWidth,
+                fit: BoxFit.cover,
+              ),
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (title != null)
+                    Text(
+                      title!,
+                      style: theme.typo.main.bodyLargeMedium.copyWith(
+                        color: theme.colors.textPrimary
+                      ),
+                    ),
+                  const SizedBox(height: 4),
+                  if (standarText != null && standarDate != null)
+                    Text.rich(
+                      TextSpan(
+                        children:[
+                          TextSpan(
+                            text: standarText,
+                            style: theme.typo.main.bodyDefaultMedium.copyWith(
+                              color: theme.colors.textTertiary
+                            ),
+                          ),
+                          TextSpan(
+                            text: standarDate,
+                            style: theme.typo.main.bodyDefaultRegular.copyWith(
+                              color: theme.colors.textTertiary
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  if (expressText != null && expressDate != null)
+                    Text.rich(
+                      TextSpan(
+                        children:[
+                          TextSpan(
+                            text: expressText,
+                            style: theme.typo.main.bodyDefaultMedium.copyWith(
+                              color: theme.colors.textTertiary
+                            ),
+                          ),
+                          TextSpan(
+                            text: expressDate,
+                            style: theme.typo.main.bodyDefaultRegular.copyWith(
+                              color: theme.colors.textTertiary
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
