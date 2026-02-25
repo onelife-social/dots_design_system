9ccd0691f23452637f90ea6138c8c46ba932fcf8
Alex Luque <alex.luque@onelife.social>
2026-02-25 17:04:53 +0100

    fixed comments

--- lib/src/components/divider/dots_divider.dart
+++ lib/src/components/divider/dots_divider.dart
@@ -4,50 +4,48 @@ import 'package:flutter/material.dart';
 class DotsDivider extends StatelessWidget {
   /// Optional custom width for the divider.
   ///
-  /// Defaults to whole width.
+  /// Defaults to full available width.
   final double? width;
 
   const DotsDivider({
     super.key,
-    this.width,
+    this.width = double.infinity,
   });
 
   @override
   Widget build(BuildContext context) {
     final theme = context.dotsTheme;
 
-    return Center(
-      child: Opacity(
-        opacity: 0.60,
-        child: Container(
-          width: width ?? double.infinity,
-          clipBehavior: Clip.antiAlias,
-          decoration: ShapeDecoration(
-            shape: RoundedRectangleBorder(
-              borderRadius: DotsBorderRadius.r1000,
+    return Opacity(
+      opacity: 0.60,
+      child: Container(
+        width: width,
+        clipBehavior: Clip.antiAlias,
+        decoration: ShapeDecoration(
+          shape: RoundedRectangleBorder(
+            borderRadius: DotsBorderRadius.r1000,
+          ),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              height: 1,
+              decoration: BoxDecoration(
+                color: theme.colors.labelSecondary,
+              ),
             ),
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                width: double.infinity,
-                height: 1,
-                decoration: BoxDecoration(
-                  color: theme.colors.labelSecondary,
-                ),
+            Container(
+              width: double.infinity,
+              height: 1,
+              decoration: BoxDecoration(
+                color: theme.colors.bgStrong,
               ),
-              Container(
-                width: double.infinity,
-                height: 1,
-                decoration: BoxDecoration(
-                  color: theme.colors.bgStrong,
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
