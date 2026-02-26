c47c59ee65062e8fa1f46e794a76310e6f293e36
juanan-onelife <juanan.garcia@onelife.social>
2026-02-26 13:10:28 +0100

    created RadioCardLabelVariant

--- example/lib/stories/stories_by_folder/miscellaneous_stories.dart
+++ example/lib/stories/stories_by_folder/miscellaneous_stories.dart
@@ -1,3 +1,4 @@
+import 'package:dots_design_system/dots_design_system.dart';
 import 'package:example/components/radio_card_icons_demo_page.dart';
 import 'package:example/components/radio_card_label_demo_page.dart';
 import 'package:storybook_flutter/storybook_flutter.dart';
@@ -6,7 +7,17 @@ List<Story> get miscellaneousStories => [
       Story(
         name: 'Miscellaneous/Radio Card Label',
         description: 'Demo page for Radio Card Label',
-        builder: (context) => RadioCardLabelDemoScreen(),
+        builder: (context) => RadioCardLabelDemoScreen(
+          variant: context.knobs.options(
+            label: 'Variant',
+            initial: RadioCardLabelVariant.small,
+            options: const [
+              Option(label: 'Small', value: RadioCardLabelVariant.small),
+              Option(label: 'Medium', value: RadioCardLabelVariant.medium),
+            ],
+          ),
+          value: context.knobs.text(label: 'Value'),
+        ),
       ),
       Story(
         name: 'Miscellaneous/Radio Card Icons',
